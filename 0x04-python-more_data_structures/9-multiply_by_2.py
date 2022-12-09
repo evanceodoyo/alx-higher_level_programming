5ffae2f9b1e24d2890e67c6bb5563b6c0147116a
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2022-12-10 01:15:01 +0300

    Use a copy of theh dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for k in a_dictionary:
-        a_dictionary[k] *= 2
-    return a_dictionary
+    tmp_dict = a_dictionary.copy()
+    for k in tmp_dict.keys():
+        tmp_dict[k] *= 2
+    return tmp_dict
