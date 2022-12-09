1e8b10dc0983fa29a9cbd969cab047f27f55f0f8
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2022-12-10 02:10:34 +0300

    Brute force

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return 'None'
-    return max(a_dictiionary, key=a_dictionary.get)
+        return None
+    highest = max(a_dictionary.values())
+    for k, v in a_dictionary.items():
+        if v is highest:
+            return k
