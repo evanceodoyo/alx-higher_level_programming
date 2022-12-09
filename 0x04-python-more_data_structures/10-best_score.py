a7ca5fed74be526772a2fcfcf1b2d3137b73c464
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2022-12-10 02:07:48 +0300

    Correct the function name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
+def best_score(a_dictionary):
     if not a_dictionary:
         return 'None'
-    return max(a_dictionary, key=a_dictionary.get)
+    return max(a_dictiionary, key=a_dictionary.get)
