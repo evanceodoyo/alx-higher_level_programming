433c9f2c0fb9220422a742d7208876247b57ed36
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2022-12-10 03:15:13 +0300

    Romans to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    roman_int = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            }
+    roman_string = roman_string.replace("IV", "III")
+    .replace("IX", "VIIII")
+    .replace("XL", "XXXX")
+    .replace("XC", "LXXXX")
+    .replace("CD", "CCCC")
+    .replace("CM", "DCCCC")
+    return sum(map(lambda x: roman_int[x], roman_string))
