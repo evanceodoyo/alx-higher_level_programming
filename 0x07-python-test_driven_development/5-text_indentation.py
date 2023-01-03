2e86721ba27547b9aea2baf7136c1f547d27f9be
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2023-01-03 17:19:55 +0300

    Indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
