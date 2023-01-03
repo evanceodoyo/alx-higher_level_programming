ed41f17192325ef787375b0589a1d480537a9313
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2023-01-03 14:07:07 +0300

    Add say_my_name and print_square

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Defines a function to add two integers"""
 
+
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
     a and are first casted to integers if they are float.
-    
+
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
@@ -12,4 +13,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-     return int(a) + int(b)
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    """Print a square with the # character.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+        TypeError: If size if a float and < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print('#' * size)
