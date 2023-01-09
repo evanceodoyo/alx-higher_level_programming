92147ad06741c44ae2ed633620ef288a435f6fb2
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2023-01-09 22:10:38 +0300

    Complete Geometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represent BaseGeometry class."""
+
+    def area(self):
+        """Raises not implemented exception."""
+        raise Exception("area() is not implemented")
