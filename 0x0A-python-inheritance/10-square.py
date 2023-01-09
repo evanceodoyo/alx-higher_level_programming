6cc32ce1a1f3fd7d70bf6a2469888668bd968adf
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2023-01-09 23:31:09 +0300

    Implement square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Implements a Square class.
+Square class inherits from Rectangle.
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square:
+    """A Square class."""
+
+    def __init__(self, size):
+        super().__init__(self.__size, self.__size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Implements a Square class.
+Square class inherits from Rectangle.
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """A Square class."""
+
+    def __init__(self, size):
+        super().__init__(self.__size, self.__size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
