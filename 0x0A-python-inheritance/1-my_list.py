828ac4f3e479b9a4bb62fdc61b11868764f91936
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2023-01-09 14:39:52 +0300

    Print list in sorted orde. And check object type/instance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""Module: 1-my_list
+""
+
+
+class MyList(list):
+    """Represents a MyList
+    """
+
+    def print_sorted(self):
+        """prints sorted list
+        """
+        print(sorted(self))
