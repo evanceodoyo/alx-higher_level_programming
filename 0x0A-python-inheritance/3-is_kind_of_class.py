ee1c039a5a8b0525083b2c7057f350ce64c88679
Evance Odoyo <85946953+evanceodoyo@users.noreply.github.com>
2023-01-09 15:04:06 +0300

    Inheritance and object type check

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' module: 3-is_kind_of_class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    '''
+    return isinstance(obj, a_class)
