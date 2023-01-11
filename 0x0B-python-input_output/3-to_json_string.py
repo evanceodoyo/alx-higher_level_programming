2e698581de7180b1b3043b28e1fbe3d277487104
Evance Odoyo <odoyoevanc@gmail.com>
2023-01-11 21:39:35 +0300

    json dumps/json loads

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns the JSON representation of an object (string)
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Return a JSON representation of obj
+    """
+    return json.dumps(obj)
