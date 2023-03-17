d6785765d869ebe468d391a18253938527133c07
Evance Odoyo <odoyoevanc@gmail.com>
2023-03-17 18:09:14 +0300

    simplify logic

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,7 @@ const sqSize = parseInt(process.argv[2], 10);
 if (isNaN(sqSize)) {
   console.log('Missing size');
 } else {
-  let sqString = 'X'.repeat(sqSize);
   for (let i = 0; i < sqSize; i++) {
-    console.log(sqString);
+    console.log('X'.repeat(sqSize));
   }
 }
