2a54721ba9a6e839f4252f81ce214ce94ecec31f
Evance Odoyo <odoyoevanc@gmail.com>
2023-03-17 17:43:55 +0300

    modify for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const l of lines) {
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let l of lines) {
   console.log(l);
 }
