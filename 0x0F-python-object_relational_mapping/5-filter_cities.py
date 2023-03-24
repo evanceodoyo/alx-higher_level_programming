b97778621b1af14426ddf3a0da8a660de1e9cafd
Evance Odoyo <odoyoevanc@gmail.com>
2023-03-24 23:59:57 +0300

    Cities that match the state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+List cities that match the query
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL
+    cursor = db.cursor()
+    sql_cmd = """SELECT cities.name
+                 FROM states
+                 INNER JOIN cities ON states.id = cities.state_id
+                 WHERE states.name LIKE %s
+                 ORDER BY cities.id ASC"""
+    cursor.execute(sql_cmd, (argv[4],))
+
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
