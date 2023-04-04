134ea259f854543a8ad0d3805f4e40d4c6e94e24
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-04 22:15:41 +0300

    Reduce number of lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Displays the size of the body of the response.
-# Usage: ./0-body_size.sh url:port
-
+# Displays the size of the body of the response; Usage: ./0-body_size.sh url:port
 curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
