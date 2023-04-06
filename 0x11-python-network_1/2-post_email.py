a8e92f9c0652b71d27b67469be99603b1eca55c5
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-07 00:08:45 +0300

    Fix spelling

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ import urllib.parse
 if __name__ == "__main__":
     values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encdoe('ascii')
+    data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         print("Your email is: {}"
