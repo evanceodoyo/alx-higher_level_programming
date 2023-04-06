23d05554bcbbb6118b17cf4a2aade9d858123f30
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-06 19:08:21 +0300

    conform to pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ Script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        the_page = response.read()
-        print("Body response:$")
-        print("\t- type: {}$".format(type(the_page)))
-        print("\t- content: {}$".format(the_page))
-        print("\t- utf8 content: {}$".format(the_page.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        the_page = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
