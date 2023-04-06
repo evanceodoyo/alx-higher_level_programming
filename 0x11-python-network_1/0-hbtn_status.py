9924021f0007c19f5bb5d53294a83f442b7e3b63
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-06 19:05:07 +0300

    add terminating character

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         the_page = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
-        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
+        print("Body response:$")
+        print("\t- type: {}$".format(type(the_page)))
+        print("\t- content: {}$".format(the_page))
+        print("\t- utf8 content: {}$".format(the_page.decode('utf-8')))
