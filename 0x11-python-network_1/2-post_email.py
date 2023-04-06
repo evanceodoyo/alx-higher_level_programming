e1f9cdc2aa24f7e483e505519d934ea131393480
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-06 23:50:54 +0300

    Make not executed when imported

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,10 +10,11 @@ import sys
 import urllib.request
 import urllib.parse
 
-values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encdoe('ascii')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as response:
-    print("Your email is: {}"
-          .format(response.read().decode('utf-8')))
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encdoe('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print("Your email is: {}"
+              .format(response.read().decode('utf-8')))
