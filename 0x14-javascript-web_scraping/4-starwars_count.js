84a437e34b2784fdf385b6c3dac48bfb24a5b286
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-26 02:01:45 +0300

    count movies by filtering character id

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiURL = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(apiURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let total = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) {
+          total += 1;
+        }
+      }
+    }
+    console.log(total);
+  }
+});
