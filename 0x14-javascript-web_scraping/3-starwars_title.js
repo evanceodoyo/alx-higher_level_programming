d98227308d2860e93b5e9fe32ff8b9ea388b9787
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-26 00:49:29 +0300

    make code semistandard compliant

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require('request');
 const movieID = process.argv[2];
 const endpoint = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-request (endpoint, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(JSON.parse(body).title);
-    }
+request(endpoint, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
