3de378210de380fc940de6cdbb4ec8fd544366ac
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-26 00:48:33 +0300

    Get Star wars movie title from a given integer

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieID = process.argv[2];
+const endpoint = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request (endpoint, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(JSON.parse(body).title);
+    }
+});
