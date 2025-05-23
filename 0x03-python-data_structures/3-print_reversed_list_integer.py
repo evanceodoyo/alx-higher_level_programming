d52b9c695cddaec2ad8393217ad5fdeb3776d808
Evance Odoyo <odoyoevanc@gmail.com>
2025-05-23 22:59:57 +0300

    fix: wrong usage of in operator

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list in None:
-        return
-    for i in  reversed(my_list):
+    for i in my_list[::-1]:
         print("{:d}".format(i))
